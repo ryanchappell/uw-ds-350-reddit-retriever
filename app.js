f83fae6cd4dca002e55c83be08d50ea0ce3b27da
ryanchappell <ryan.p.chap@gmail.com>
2016-04-25 20:37:43 -0700

    initial retrieval code
    - logging
    - config
    - get "hot" results, write to file

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('hello world!')
+'use strict';
+const snoowrap = require('snoowrap');
+const fs = require('fs');
+const winston = require('winston');
+const apiConfig = require('./apiConfig.js');
+
+// log config
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: 'retriever.log' })
+  ]
+});
+
+const r = new snoowrap(apiConfig.apiConfig);
+//r.config({debug: true});
+
+logger.info('starting up!');
+
+// e.g. get front page listings (posts)
+r.get_hot()
+.then(function(result){
+  var fileDate = new Date().toString();
+  // TODO: somethin' like this
+  //var fileName = 'front-page-hot_' + fileDate.format("%Y-%m-%d %H:%M:%S") + '.json';
+  var fileName = 'result.json'
+
+  fs.writeFile(fileName, JSON.stringify(result, null, '  '));
+})
+.catch(function(err){
+  console.log('error occurred: ' + err);
+});
+
+logger.info('done!');
