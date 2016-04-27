66c9896401f1cc8639de9529f362b5b23a438f22
ryanchappell <ryan.p.chap@gmail.com>
2016-04-26 21:18:11 -0700

    cl args, rearranging interval

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const apiConfig = require('./apiConfig.js');
 const resultWriter = require('./resultWriter.js');
 const auth = require('./auth');
 
+if (process.argv < 3 ||
+  (process.argv[2] != 1 && process.argv[2] != 2))
+{
+  console.log('usage: node app.js [DATAITEMID]')
+  console.log('[DATAITEMID] can be 1 for "hot",  2 for "new"');
+  process.exit();
+}
+
 // log config
 var logger = new (winston.Logger)({
   transports: [
@@ -14,44 +22,53 @@ var logger = new (winston.Logger)({
   ]
 });
 
-auth.getAccessToken(function(token){
-  if (!token)
-  {
-    logger.error('token is null');
-    process.exit();
-  }
+var dataItem = process.argv[2];
+var retrievalIntervalMillis = 5 * 1000;
 
-  const r = new snoowrap({
-    user_agent: apiConfig.apiConfig.user_agent,
-    access_token: token
-  });
-  const retrievalIntervalMillis = 60 * 1000;
-  //r.config({debug: true});
+logger.info("starting up!");
+logger.info("retrievalIntervalMillis: " + retrievalIntervalMillis);
 
-  logger.info('starting up!');
+setInterval(function(){
 
-  setInterval(function(){
-    r.get_hot()
-    .then(function(result){
-      resultWriter.write('hot',result);
-      logger.info('wrote "hot" result');
-    })
-    .catch(function(err){
-      logger.error('error occurred in "hot": ' + err);
+  auth.getAccessToken(function(token){
+    if (!token)
+    {
+      logger.error('token is null');
+      process.exit();
+    }
+
+    console.log('got token ' + token);
+
+    var r = new snoowrap({
+      user_agent: apiConfig.apiConfig.user_agent,
+      access_token: token
     });
-  }, retrievalIntervalMillis);
 
-  setInterval(function(){
-    r.get_new()
-    .then(function(result){
-      resultWriter.write('new',result);
-      logger.info('wrote "new" result');
-    })
-    .catch(function(err){
-      logger.error('error occurred in "new": ' + err);
-    });
-  }, retrievalIntervalMillis);
+    switch(dataItem)
+    {
+        case "1":
+          r.get_hot()
+          .then(function(result){
+            resultWriter.write('hot',result);
+            logger.info('wrote "hot" result');
+          })
+          .catch(function(err){
+            logger.error('error occurred in "hot": ' + err);
+          });
+          break;
+        case "2":
+          r.get_new()
+          .then(function(result){
+            resultWriter.write('new',result);
+            logger.info('wrote "new" result');
+          })
+          .catch(function(err){
+            logger.error('error occurred in "new": ' + err);
+          });
+          break;
+        default:
+          throw new Error("not implemented for dataItem " + dataItem);
+    }
 
-});
-
-logger.info('done!');
+  })
+}, retrievalIntervalMillis);
