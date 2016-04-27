392ebcf08acb5f4d4d3617cd0f4fad4f36ae5cf7
ryanchappell <ryan.p.chap@gmail.com>
2016-04-26 21:37:34 -0700

    reducing interval

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var logger = new (winston.Logger)({
 });
 
 var dataItem = process.argv[2];
-var retrievalIntervalMillis = 5 * 60 * 1000;
+var retrievalIntervalMillis = 60000;
 
 logger.info("starting up!");
 logger.info("retrievalIntervalMillis: " + retrievalIntervalMillis);
